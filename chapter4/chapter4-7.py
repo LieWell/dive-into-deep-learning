4852fc5e51c5db9f15b3652927307eac9f22aba3
liewell <liewell1011@gmail.com>
2024-04-16 15:08:08 +0800

    add chapter 4.7

---
+++ chapter4/chapter4-7.py
@@ -0,0 +1,38 @@
+"""
+chapter 4.7 前向传播、反向传播和计算图
+TODO  这一节太难了,作为了解,后面补充
+"""
+
+import math
+import numpy as np
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+def four_seven_one():
+    """
+    chapter 4.7.1 前向传播(forward propagation)
+    按顺序(从输入层到输出层)计算和存储神经网络中每层的结果。
+    """
+    pass
+
+
+def four_seven_two():
+    """
+    chapter 4.7.2 计算图(computational graph)
+    参考文件 functions/computational_graph.py
+    """
+    pass
+
+
+def four_seven_three():
+    """
+    chapter 4.7.3 反向传播(backward propagation)
+    根据微积分中的链式规则,按相反的顺序从输出层到输入层遍历网络。。
+    """
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ functions/computational_graph.py
@@ -0,0 +1,7 @@
+"""
+计算图(computational graph)
+是一种有向无环图（directed acyclic graph，DAG）。
+计算图用节点表示变量，用有向边（directed edge）表示计算。
+有向边的目的节点称为子节点，源节点称为父节点，计算图定义如何用父节点计算子节点。
+"""
+
